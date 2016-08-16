abab7bd46a96448ebe71f65345f7dbe8fb63d6e2
Steven P. Goldsmith <sgjava@gmail.com>
2016-08-15 21:21:57 -0400

    Fix script

--- pine64/finish.sh
+++ pine64/finish.sh
@@ -39,12 +39,12 @@ log(){
 
 # Copy /boot 
 log "Copy /boot"
-cp -R "/boot/*" "$builddir/boot/."
+cp -r "/boot/*" "$builddir/boot/."
 
 # Copy /lib/modules 
 log "Copy /lib/modules"
 mkdir -p "$builddir/lib/modules"
-cp -R "/lib/modules/*" "$builddir/lib/modules/."
+cp -r "/lib/modules/*" "$builddir/lib/modules/."
 
 # Copy /etc/fstab 
 log "Copy /etc/fstab"
